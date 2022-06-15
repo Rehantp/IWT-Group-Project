e9665e5459ee35e6fcf560eb4e6c25a0c9724bcc
Rehan Perera <95935466+Rehantp@users.noreply.github.com>
2022-06-15 08:58:12 +0530

    JS

---
+++ js/tg.js
@@ -0,0 +1,19 @@
+function displaymsg() {
+    document.getElementById("hiddenMesseage").style.display = 'block'
+}
+
+function displaymsg1() {
+    document.getElementById("hiddenMesseage1").style.display = 'block'
+}
+
+function displaymsg2() {
+    document.getElementById("hiddenMesseage2").style.display = 'block'
+}
+
+function displaymsg3() {
+    document.getElementById("hiddenMesseage3").style.display = 'block'
+}
+
+function displaymsg4() {
+    document.getElementById("hiddenMesseage4").style.display = 'block'
+}
